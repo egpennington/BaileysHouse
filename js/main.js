fc131ef1d719ca79274522951a8ec5fda02efe79
egpennington <egpennington@hotmail.com>
2023-12-11 19:33:46 -0600

    Added grid and html css
    Got page set up, HTML and CSS

--- js/main.js
+++ js/main.js
@@ -1,8 +1,54 @@
-const btnEl = document.getElementById("btn-el")
-const message = document.getElementById("message-el")
-
-btnEl.addEventListener("click", () => {
-    console.log("hello, i'm alive")
-    message.textContent = "Hello, I'm alive"
-
-})
+const accounts = [
+    {
+        id: 1,
+        title: "Main Account",
+        balance: "6700.56",
+        spendings: [
+            {
+                category: "Rent",
+                spent: "1450"
+            },
+            {
+                category: "Groceries",
+                spent: "564"
+            },
+            {
+                category: "Restaurants",
+                spent: "123"
+            },
+            {
+                category: "Transport",
+                spent: "81"
+            },
+            {
+                category: "Internet",
+                spent: "50"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Expenses",
+        balance: "5134.63",
+        spendings: [
+            {
+                category: "Netflix",
+                spent: "19.99"
+            },
+            {
+                category: "HBO Max",
+                spent: "14.99"
+            },
+            {
+                category: "Setapp",
+                spent: "9.99"
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Savings",
+        balance: "36500.12",
+        spendings: []
+    }
+]
